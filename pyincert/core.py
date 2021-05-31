26c49435619a7f4019ee5b01ff80842cf1661158
Mateo Contenla <mcontenlaf@gmail.com>
2021-05-31 16:46:52 -0400

    Reescribiendo todo desde 0 :(

--- pyincert/core.py
+++ pyincert/core.py
@@ -1,33 +1,47 @@
 import math
+import sys
 
-__all__ = ["V", "ER", "er"]
+# __all__ = ["V", "ER", "er"]
 
-# En general, si el resultado R es una función de medidas (x, y, ...) en donde
-# R = f(x, y, ...), la fórmula general para la propagación de errores es:
-#  δR = sqrt((∂R/∂x × δx)^2 + (∂R/∂y × δy)^2 + ...)
-# Notando que ∂R/∂x es la notación para la derivada parcial y δx el error
-# asociado a la variable.
-# De acá se deducirán todas las fórmulas.
+from .util import lambdify, jacobiana
+
+# =========== Fin de los imports ==========
 
 # Devuelve true si obj es un int o un float, false en caso contrario
 def es_numero(obj):
     return isinstance(obj, (int, float)) and not isinstance(obj, bool)
 
-# Esta función es media compleja de explicar. Lo que pasa es que la fórmula
-# general real del error propagado de una función de dos variables es:
-# δf(x, y) = sqrt((∂R/∂x × δx)^2 + (∂R/∂y × δy)^2 + 2(∂R/∂x)(∂R/∂y)ρδyδx)
-#                                                   ~~~~~~~~~~~~~~~~~~~
-# Es básicamente un cuadrado de binomio, pero ese término subrayado depende de
-# la correlación de las variables x e y. Si x e y son variables no
-# correlacionadas, ρδxδy = 0, quedando solo la suma de los cuadrados.
-# Entonces, esta función me devuelve 0 si v1 y v2 no están correlacionados, y
-# 2(∂R/∂x)(∂R/∂y)ρδyδx si lo están (v1 is v2, en python).
-# La llamo sumando central solo porque cuando uno desarrolla un cuadrado de
-# binomio a mano, el sumando "2ab" queda al centro.
-def sumando_central(v1, v2, dfdx, dfdy):
-    if v1 is v2:
-        return 2 * dfdx * dfdy* v1.error * v2.error
-    return 0
+def crear_binop(func):
+    dx, dy = jacobiana(func)
+
+    def operar(self, otro):
+        if es_numero(otro):
+            return operar(self,  V(otro))
+
+        dx_eval = dx(self.valor, otro.valor)
+        dy_eval = dy(self.valor, otro.valor)
+
+        res = func(self.valor, otro.valor)
+
+        incert = math.sqrt(pow(dx_eval * self.desv_est, 2) + pow(dy_eval * otro.desv_est, 2))
+
+        obj = V(res, incert)
+        obj.comb_lineal = self.comb_lineal
+        obj.add_comb_lin((self, dx_eval))
+        obj.add_comb_lin((otro, dy_eval))
+        return obj
+    return operar
+
+def agregar_dunders_a_V():
+    ops_ref = {
+        "add": lambda x, y: x + y,
+        "sub": lambda x, y: x - y,
+        "mul": lambda x, y: x * y,
+        "truediv": lambda x, y: x / y,
+    }
+
+    for nombre, operacion in ops_ref.items():
+        setattr(V, f"__{nombre}__", crear_binop(operacion))
 
 class ER:
     """
@@ -60,23 +74,30 @@ class V:
     # Este valor afecta únicamente la representación de la clase en __repr__.
     # Internamente sigue siendo la misma cantidad. Para modificar este valor es
     # necesario usar el método V.cantdec.
-    cant_decimales = 4
+    cant_decimales = 15
 
     # Este es el separador que se usará en el método __repr__. Puede ser
     # cambiada por "+/-" con el método de clase V.unicode(false) por si la
     # consola no soporta unicode.
     pm = "±"
 
-    def __init__(self, valor, error = 0):
+    def __init__(self, valor, desv_est = 0):
         """
         Inicializa la clase y arroja un error si alguno de los parámetros
         (valor o error) no es un número.
         """
 
-        if not es_numero(valor) or not (es_numero(error) or isinstance(error, ER)):
+        if not es_numero(valor) or not (es_numero(desv_est) or
+                                        isinstance(desv_est, ER)):
             raise TypeError("Eso no es un número a :(")
-        self.valor = valor
-        self.error = self.valor * error.valor if isinstance(error, ER) else error
+        self.valor = float(valor)
+
+        if isinstance(desv_est, ER):
+            desv_est = self.valor * desv_est.valor
+        self.desv_est = desv_est
+
+        self.comb_lineal = {}
+
 
     @classmethod
     def cant_dec(cls, num):
@@ -91,141 +112,11 @@ class V:
         else:
             cls.pm = "+/-"
 
-    def error_relativo(self):
-        return self.error / self.valor
+    def add_comb_lin(self, elem):
+        self.comb_lineal[elem[0]] = elem[1]
 
     def __repr__(self):
         """Representación bonita :)"""
-        return f"{round(self.valor, self.cant_decimales)} {V.pm} {round(self.error, self.cant_decimales)}"
+        return f"{round(self.valor, self.cant_decimales)} {V.pm} {round(self.desv_est, self.cant_decimales)}"
 
-    def __add__(self, otro):
-        """
-        Devuelve un objeto V correspondiente a la suma del objeto actual y otro
-        objeto V o un número.
-
-        Ejemplos:
-        >>> V(3, 0.01) + 2 + 3 + V(8)
-        """
-
-        if es_numero(otro):
-            return self + V(otro)
-
-        s = self.valor + otro.valor
-        return V(s, math.sqrt(pow(self.error, 2)
-                              + pow(otro.error, 2)
-                              + sumando_central(self, otro, 1, 1)))
-
-    def __radd__(self, otro):
-        """
-        Devuelve un objeto V correspondiente a la suma entre un número y el
-        objeto actual.
-
-        Ejemplo:
-        >>> 4 + V(3, 1e-2)
-        """
-
-        return self + otro
-
-    def __sub__(self, otro):
-        """
-        Devuelve un objeto V correspondiente a la resta entre el objeto actual
-        y el argumento.
-
-        Ejemplo:
-        >>> V(3, 1e-5) - 8
-        """
-
-        if es_numero(otro):
-            return self - V(otro)
-
-        r = self.valor - otro.valor
-        return V(r, math.sqrt(pow(self.error, 2)
-                              + pow(otro.error, 2)
-                              + sumando_central(self, otro, 1, 1)))
-
-    def __rsub__(self, otro):
-        """
-        Devuelve un objeto V correspondiente a la operación de un número menos
-        el objeto actual.
-
-        Ejemplo:
-        >>> 12.8 - V(4, 9)
-        """
-
-        return V(otro) - self
-
-    def __mul__(self, otro):
-        """
-        Devuelve un objeto V correspondiente a la multiplicación del objeto
-        actual con otro objeto V o un número.
-
-        Ejemplo:
-        >>> V(2, 1e-4) * 3 * V(4)
-        """
-
-        if es_numero(otro):
-            # Podría hacer otro = V(otro), pero se me desordenan los tipos en
-            # la cabeza
-            return self * V(otro)
-
-        m = self.valor * otro.valor
-        return V(m, math.sqrt(pow(self.valor * otro.error, 2)
-                              + pow(otro.valor * self.error, 2)
-                              + sumando_central(self, otro, self.valor, otro.valor)))
-
-
-    def __rmul__(self, otro):
-        """
-        Devuelve un objeto V correspondiente a la multiplicación entre un
-        número y el objeto actual.
-
-        Ejemplo:
-        >>> 2.5 * V(7.99993, 3e-89)
-        """
-
-        return V(otro) * self
-
-    def __truediv__(self, otro):
-        """
-        Devuelve un objeto V correspondiente a la división del objeto actual
-        con otro objeto V o un número.
-
-        Ejemplo:
-        >>> V(421, 68) / 3 / V(6, 2)
-        """
-
-        if es_numero(otro):
-            return self / V(otro)
-
-        d = self.valor / otro.valor
-        return V(d, math.sqrt(pow(self.error / otro.valor, 2)
-                              + pow(self.valor * otro.error / otro.valor**2 ,2)
-                              + sumando_central(self, otro, 1/otro.valor,
-                                                -self.valor/(otro.valor)**2)))
-
-    def __rtruediv__(self, otro):
-        """
-        Devuelve un objeto V correspondiente a la división de un número con el
-        objeto actual.
-
-        Ejemplo:
-        >>> 6 / V(8, 9.7776)
-        """
-
-        return V(otro) / self
-
-    def __pow__(self, exponente):
-        """
-        Devuelve un objeto V correspondiente al objeto actual elevado a otro
-        objeto V o un número.
-
-        Ejemplo:
-        >>> V(7, 9.886) ** 2 ** V(7)
-        """
-
-        # TODO: Permitir elevar un objeto V a otro objeto V. Basta con sacar
-        # las derivadas parciales de f(x, y) = x^y, pero es tarde y me da lata.
-        # :)
-        e = self.valor ** exponente
-        return V(e, abs(exponente * (self.valor ** (exponente - 1)) * self.error))
-
+agregar_dunders_a_V()
--- pyincert/util.py
+++ pyincert/util.py
@@ -1,5 +1,160 @@
 """Funciones comunes"""
 
+import functools
+import math
+import sys
+
+# Aquí definimos la función que calcula derivadas parciales. Primero creamos
+# una implementación que usa diferenciación numérica, en específico una
+# aproximación con diferencias finitas centrales.
+# (https://en.wikipedia.org/wiki/Numerical_differentiation
+# Ese no es el caso óptimo eso sí. Después intentamos importar sympy. Si falla,
+# dejamos la función implementada con diferencia fínita; si se importa
+# correctamente redefinimos la función para calcula derivadas parciales
+# (parcial) para que haga uso de computación simbólica. Esto es mejor porque en
+# lugar de hacer una aproximación del tipo (f(x + eps) - f(x - eps))/2eps,
+# directamente me devuelve la función correspondiente a la derivada.
+
+# Los floats de python son de doble precisión, por lo que un buen candidato
+# al valor del step_size (h) es el sqrt(epsilon de la máquina) * abs(x)
+STEPSIZE = math.sqrt(sys.float_info.epsilon)
+
+# Implementamos la diferencia central manualmente
+def parcial(func, par_index):
+    """
+    Devuelve una función correspondiente a la derivada parcial de func con
+    respecto a sus parametros[par_index].
+
+    func: una función con parámetros (x, y, ...).
+
+    par_index: un entero correspondiente al índice de la variable en los
+    parámetros de la función.
+    """
+
+    params = func.__code__.co_varnames
+
+    if par_index < 0 or par_index > len(params) - 1:
+        err_s = f"'{func.__name__}' does not have a parameter of index {par_index}"
+        raise TypeError(err_s)
+
+    var_name = params[par_index]
+
+    @lambdify(params)
+    def f_derivative(**valores):
+        step = STEPSIZE * abs(valores[var_name])
+
+        valores[var_name] += step
+        f_mas_eps = func(**valores)
+
+        valores[var_name] -= 2 * step
+        f_menos_eps = func(**valores)
+
+        return (f_mas_eps - f_menos_eps)/(2 * step)
+
+    return f_derivative
+
+try:
+    import sympy
+except ImportError:
+    # sympy no está instalado
+    # la función parcial queda como la implementación de diferencia central
+    pass
+
+else:
+    # sympy está instalado
+    parcial_diff = parcial
+
+    # Esta implmentación siempre será la preferida, ya que computa la derivada
+    # simbolicamente.
+    def parcial(func, par_index):
+        """
+        Devuelve una función correspondiente a la derivada parcial de func con
+        respecto a sus parametros[par_index].
+
+        func: una función con parámetros (x, y, ...).
+
+        par_index: un entero correspondiente al índice de la variable en los
+        parámetros de la función.
+        """
+        params = func.__code__.co_varnames
+
+        if par_index < 0 or par_index > len(params) - 1:
+            err_s = f"'{func.__name__}' does not have a parameter of index {par_index}"
+            raise TypeError(err_s)
+
+        symbol_params = [sympy.Symbol(p) for p in params]
+
+        try:
+            symbol_func = func(*symbol_params)
+        except TypeError:
+            # Si la derivada calculada simbólicamente falla (porque usa
+            # funciones externas por ejemplo), simplemente la cálculamos con
+            # aproimaciones de diferencias finitas.
+            return parcial_diff(func, par_index)
+
+        symbol_diff = sympy.diff(symbol_func, symbol_params[par_index])
+        return sympy.lambdify(symbol_params, symbol_diff)
+
+# Se explica sola. Si no es así, vuelve revisar tus apuntes de CVV.
+# Esta aplica solamente para funciones definidad de R_n -> R. Por esto mismo
+# calculo solamente la primera fila. ¿Podría generalizar la función? Si, ¿Me da
+# lata hacerlo? también.
+def jacobiana(func):
+    arity = func.__code__.co_argcount
+    m_jacobiana = list()
+
+    for index in range(arity):
+        m_jacobiana.append(parcial(func, index))
+
+    return m_jacobiana
+
+
+# Esta función me costó más de lo que me gutaría admitir.
+# Voy a dar un ejemplo medio simple porque hice esta función para un tema super
+# específico.
+#
+# Tengo una función de este estilo
+# outer_f = lambda x, y: x * y
+#
+# Y otra de este estilo:
+# def func(**kwargs):
+#     return outer_f(**kwargs) * 2
+#
+# Yo quiero usar "func" como un wrapper para "outer_f", pero "func" no tiene
+# idea de cuáles ni cuántos argumentos recibe "outer_f". Para solucionar eso,
+# defino "func" así:
+#
+# @lambdify(outer_f.__code__.co_varnames)
+# def func(**kwargs):
+#     return outer_f(**kwargs)
+#
+# Ahora puedo llamar a "func" y esta se va a asegurar de que reciba la cantidad
+# exacta de argumentos, incluso como keywords: func(x=9, y=8).
+# Si la cantidad o los nombres de los argumentos están mal, seleventará un
+# TypeError como cualquier función lo haría normalmente.
+def lambdify(param_names):
+    """
+    Decorador para forzar que una función con parámetros *args, **kwargs o
+    ambos, tenga una aridad de len(param_names).
+    """
+
+    def dec_lambdify(func):
+        def wrapper_lambdify(*args, **kwargs):
+            param_string = ", ".join(param_names)
+            args_string = ", ".join([f"{x}={x}" for x in param_names])
+            func_name = func.__name__
+            f_string = f"lambda {param_string}: {func_name}({args_string})"
+            f_lambda = eval(f_string, {func_name: func})
+
+            if len(param_names) != len(args):
+                raise TypeError(f"{func_name}() takes {len(param_names)} positional arguments but {len(args)} were given")
+
+            arg_union = {**dict(zip(param_names, args)), **kwargs}
+
+            return f_lambda(**arg_union)
+        return wrapper_lambdify
+    return dec_lambdify
+
 def exportador():
     """
     Crea un arreglo __all__ y un decorador que añade las definiciones al
@@ -18,3 +173,4 @@ def exportador():
             all_.append(obj.__name__)
         return obj
     return dec, all_
+
